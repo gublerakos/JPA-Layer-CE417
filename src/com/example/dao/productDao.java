7b01ca803b0a0da50beaa97afc430b7d9f24d1a0
gublerakos <mariaapntz@gmail.com>
2020-11-10 17:38:15 +0200

    source code

---
+++ src/com/example/dao/productDao.java
@@ -0,0 +1,120 @@
+package com.example.dao; 
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.example.model.product;
+import com.example.utl.HibernateUtil;
+
+
+public class productDao {
+
+    // Save product
+    public void saveProduct(product pr) {
+        Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.save(pr);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+    }
+
+
+    // Update product
+    public void updateProduct(product pr) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.update(pr);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+    }
+    
+    // Delete product
+    public void deleteProduct(String barcode) {
+
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+
+            // Delete a product object
+            product pr = session.get(product.class, barcode);
+			if (pr != null) {
+				session.delete(pr);
+				System.out.println("product is deleted");
+			}
+
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+    }
+    
+    // Get product
+    public product getProduct(String barcode) {
+
+		Transaction transaction = null;
+		product pr = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// get a product object
+			pr = session.get(product.class, barcode);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		return product;
+    }
+    
+    // Get all products
+    public List<product> getAllProducts() {
+
+		Transaction transaction = null;
+		List<product> listOfProducts = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// get an product object
+			
+			listOfProducts = session.createQuery("from products").getResultList();
+			
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+		return listOfProducts;
+	}
+
+}
---
+++ src/com/example/web/productServlet.java
@@ -0,0 +1,52 @@
+package com.example.web;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.model.product;
+import com.example.dao.productDao;
+
+public class productServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    private productDao prDao;
+
+    public void init() {
+		prDao = new productDao();
+	}
+
+    public productServlet() {
+        super();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        String barcode = request.getParameter("barcode");
+        String name = request.getParameter("name");
+        String colour = request.getParameter("colour");
+        String description = request.getParameter("description");
+
+        product pr = new product();
+        pr.setBarcode(barcode);
+        pr.setName(name);
+        pr.setColour(colour);
+        pr.setDescription(description);
+        pr.setResult(0);
+
+        productDao.saveUser(newUser);        
+        
+        request.setAttribute("styles", pr);
+		RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+		view.forward(request, response);
+    }
+
+}
